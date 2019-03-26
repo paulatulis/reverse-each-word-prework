19fccb98ab201d335f764ae7a79febb34a8b2b7a
Paula Tulis <tulis.paula@gmail.com>
2019-03-26 00:48:10 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,7 +8,7 @@ def reverse_each_word(string)
 end
 array.join(" ")
 end 
-
+end 
 #reverses all the words in a string without reversing the order of the words
 #reverses all the words in another string without reversing the order of the words
 #uses .collect
